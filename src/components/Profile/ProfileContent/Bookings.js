8ff18e4772529f3e8e3224d1e01bc101f6194fbc
Hemanth R <hemanthrt18@gmail.com>
2022-02-11 20:30:38 +0530

    🧳

--- src/components/Profile/ProfileContent/Bookings.js
+++ src/components/Profile/ProfileContent/Bookings.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useContext } from 'react'
+import { IoRemoveCircleOutline } from "react-icons/io5";
 
 import { db } from '../../../firebase/firebase';
 import './ProfileContent.css'
@@ -22,6 +23,12 @@ function Bookings() {
             )
         ))
     }, [currentUser.uid]);
+
+
+    const deleteBooking = (id) => {
+        db.collection('users').doc(currentUser.uid).collection('bookings').doc(id)  
+        .delete()  
+    }
     
 
     return (
@@ -32,11 +39,12 @@ function Bookings() {
             <div className='bookings__container'>
                 {bookings.map((book) => (
                     <div className='booked_card' key={book.id}>
+                        <IoRemoveCircleOutline onClick={() => { deleteBooking(book.id) }} className='remove__booking_icon'/>
                         <div className='booked__left'>
                             <h1>{book.data.destination}</h1>
                             <h3>{book.data.location}</h3>
                         </div>
-                        <div className='booked__left'>
+                        <div className='booked__right'>
                             <h1>x{book.data.people}</h1>
                             <h3>₹{book.data.amount}</h3>
                         </div>
